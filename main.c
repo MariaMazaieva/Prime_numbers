829b0ee8f0e5b557ee2402b16fcc2678a70c74f4
MariaMazaieva <mazaevamasha@gmail.com>
2024-11-09 21:05:48 +0100

    Working version 5b

--- main.c
+++ main.c
@@ -5,26 +5,24 @@
 #define UPPER_BOUND 1000000
 #define MIN_VALUE 0
 #define size_B 100000
-// #define MAX_VALUE 991350783549
-//999983
 
 int sieve_of_Eratosthenes(int B[]);
 void division(int size, int B[], long number);
 int* control_input(int *ret, int count_arr,int B[] );
-// bool is_in_B(long n, int B[], int count_arr);
 
 int main(void) {
     int ret = EXIT_SUCCESS;
     int count_arr = 0;
     int B [size_B] = {0};
-    count_arr = sieve_of_Eratosthenes(B);
+    count_arr = sieve_of_Eratosthenes(B); // from func sieve_.. returns index of prime numbers in arr B
     control_input(&ret, count_arr, B);
     return ret;
 }
 /*Function reads numbers from stdin to variable number in a loop while, stops if number == 0.
  * @var result  - used to read amount(1) of successfully scanned numbers from input.
  * @ret - EXIT_SUCCESS, otherwise number of error if integer was not read.
- * MIN_VALUE is set to 0, MAX_VALUE is set to 991350783549
+ * MIN_VALUE is set to 0
+ * func division is called for every number
  */
 
 int* control_input(int *ret, int count_arr,int B []) {
@@ -61,8 +59,8 @@ int* control_input(int *ret, int count_arr,int B []) {
  */
 
 int sieve_of_Eratosthenes (int B[]) {
-    bool A[UPPER_BOUND] ; // to count last one as well
-    //
+    bool A[UPPER_BOUND] ;
+    // 0 and 1 is set to false, as they are not primes
     A[0] = false;
     A[1] = false;
     for (int i = 2; i < UPPER_BOUND; i++) //from 2, so 0 and 1 wont be in the array
@@ -71,48 +69,49 @@ int sieve_of_Eratosthenes (int B[]) {
     int index = 0;
     int sqr = (int)sqrt(UPPER_BOUND);
     for(int i = 2; i <UPPER_BOUND; i++) {
+        //multiples every prime number in an array to mark as false, more at the end
         if (A[i] == true && i <= sqr) {
             for(int j = i*i; j < UPPER_BOUND; j +=i) {
                 A[j] = false;
             }
         }
         if (A[i]) {
-            B[index++] = i;
+            B[index++] = i; //fill arr B with primes
         }
     }
-    //  counter = sizeof(B) / sizeof(B[0]);
-    // *count_arr = counter;
     return index;
 }
 
 /*Function divides number to the prime numbers, count its factorials as well.
  * @size  - amount of prime numbers we have from func sieve_of_Eratosthenes
+ * @p - parameter, prime number from arr B that we are testing
+ * @power - counts how many times out p was included in @number (power of a number)
+ * @first_number - bool value to track first divisor in a number
  */
 
 void division(const int size,int B[],long number) {
     bool first_number = true;
     for (int i = 0; i <size -1 && number > 1; i++){
-        int n = B[i];
-        // if (n == 0) break;
-        if( number % n == 0) {
+        int p = B[i];
+        if( number % p == 0) {
             int power = 1;
-            number = number / n;
-            while (number % n == 0) {
-                number = number / n;
+            number = number / p;
+            while (number % p == 0) {
+                number = number / p;
                 power++;
             }
             if(first_number) {
-                printf("%d",n);
+                printf("%d",p);
                 first_number = false;
             }else {
-                printf(" x %d", n);
+                printf(" x %d", p);
             }
-            if (power > 1) {
+            if (power > 1) { //prints power of a number
                 printf("^%d", power);
             }
         }
     }
-    if (number > 1) {
+    if (number > 1) { //if number can be still divided
         if (!first_number) {
             printf(" x ");
         }
