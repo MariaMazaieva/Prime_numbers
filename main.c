4be2c7d4f1a9d258ae1d2a0ca29aa4c440030f48
MariaMazaieva <mazaevamasha@gmail.com>
2024-11-09 13:37:49 +0100

    Version passed test, timeout

--- main.c
+++ main.c
@@ -1,25 +1,22 @@
-        #include <math.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #define UPPER_BOUND 1000000
-// #define MIN_VALUE 0
+#define MIN_VALUE 0
 // #define MAX_VALUE 991350783549
+
+
 int sieve_of_Eratosthenes(long n);
 void division(int size, int B[ ], long number);
-void check_for_prime(bool A[]);
-void array_of_primes(int B[ ], bool A []);
+void check_for_prime(bool A[], long * count_false);
+void array_of_primes(int B[ ], const bool A []);
 int* control_input(int *ret);
 void print_numbers(int * counter, int * power, int divisor);
 
 int main(void) {
-    // long number = -1;
     int ret = EXIT_SUCCESS;
     control_input(&ret);
-    // if ( ret == EXIT_SUCCESS ) {
-    //     sieve_of_Eratosthenes(number);
-    // }
-
     return ret;
 }
 /*Function reads numbers from stdin to variable number in a loop while, stops if number == 0.
@@ -34,12 +31,12 @@ int* control_input(int *ret) {
         int result = scanf("%ld", &number);
         if (number == 0)
             break;
-        //
-        // if (number < MIN_VALUE || number > MAX_VALUE) {
-        //     fprintf(stderr, "Error: Chybny vstup!\n");
-        //     *ret = 100;
-        //     return ret;
-        // }
+
+        if (number < MIN_VALUE ) {
+            fprintf(stderr, "Error: Chybny vstup!\n");
+            *ret = 100;
+            return ret;
+        }
         if(result !=1) {
             fprintf(stderr, "Error: Chybny vstup!\n");
             *ret = 100;
@@ -63,6 +60,7 @@ int* control_input(int *ret) {
 
 int sieve_of_Eratosthenes (long n) {
     bool A[UPPER_BOUND] ; // to count last one as well
+
     A[0] = false;
     A[1] = false;
     for (int i = 2; i < UPPER_BOUND; i++) //from 2, so 0 and 1 wont be in the array
@@ -70,15 +68,21 @@ int sieve_of_Eratosthenes (long n) {
     //set first and second to false
     int count_arr = 0;
     // int size = sizeof(A)/sizeof(A[]);
-    check_for_prime(A);
+    long count_false = 0;
+    check_for_prime(A, &count_false);
+    // printf("count false %lu\n", count_false);
+    count_arr = UPPER_BOUND - count_false-2;
     // printf("primes %d",check_for_prime(A));
-    for( int i = 0; i < UPPER_BOUND; i++) {//count the amount of prime numbers for arr B
-        if(A[i] == true)
-            count_arr++;
-    }
+    // for( int i = 0; i <= UPPER_BOUND; i++) {//count the amount of prime numbers for arr B
+    //     if(A[i])
+    //         count_arr++;
+    // }
     // printf("Array %d\n",count_arr);
-    int B[count_arr]; //creating arr B, size of amount of p
+    // printf("count false %lu\n", count_false);
+    // printf("count arr %d\n", count_arr);
+    int B[count_arr-1]; //creating arr B, size of amount of p
     array_of_primes(B,A);
+    // count_arr = sqrt(count_arr);
     division(count_arr,B, n);
 
     return 0;
@@ -87,69 +91,69 @@ int sieve_of_Eratosthenes (long n) {
 /*Function divides number to the prime numbers, count its factorials as well.
  * @size  - amount of prime numbers we have from func sieve_of_Eratosthenes
  */
-void division(int size,int B[],long number) {
+
+void  check_for_prime(bool A[], long * count_false) {
+    *count_false = 0;
+    int sqr = (int)sqrt(UPPER_BOUND);
+    for(int i = 2; i < sqr; i++) {
+        for(int j = i*i; j < UPPER_BOUND; j +=i) {
+            if (A[j] == true) {
+                A[j] = false;
+                (*count_false)++;
+            }
+        }
+    }
+}
+
+void array_of_primes(int B[], const bool A[]) {
+    int index = 0;
+    for( int i = 0; i < UPPER_BOUND; i++) {
+        if(A[i] ) {
+            B[index++] = i;
+            // index++;
+        }
+    }
+}
+
+/*
+    int
+    for( int i = 0; i <= UPPER_BOUND; i++) {//count the amount of prime numbers for arr B
+        if(A[i])
+        count_arr++;
+    }
+    // printf("Array %d\n",count_arr);
+    int B[count_arr-1]; //creating arr B, size of amount of p
+    array_of_primes(B,A);
+    division(count_arr,B, n);
+*/
+void division(const int size,int B[],long number) {
     int counter = 0;
-    int power = 1;
-    int i =0;
-    while(i < size && number > 0) {
+    // int power = ;
+    bool first_number = true;
+    // size == sqrt(size);
+    for (int i = 0; i < size && number > 1; i++){
         if( number % B[i] == 0) {
+            int power = 1;
             counter++;
             number = number / B[i];
             while (number % B[i] == 0) {
                 number = number / B[i];
                 power++;
             }
-            print_numbers(&counter, &power, B[i]);
-        }
-        i++;
-    }
-    if(number > 0)
-        putchar('\n');
-}
-
-/*Function prints prime numbers of a given number.
- * has special conditions for each case of a number and its position in a line
- */
-
-void print_numbers(int* counter, int * power, int divisor) {
-    // reinitialize power, so it won't affect the next number
-    if (*counter == 1 && *power > 1) //special condition for first !prime number
-    {printf("%d^%d", divisor,*power);*power = 1;}
-
-    else if (*counter == 1 && *power == 1) //special condition for first prime number
-        printf("%d", divisor);
-
-    else if (*counter >1  && *power > 1) //special condition for !first !prime number
-    {printf(" x %d^%d",divisor, *power);*power = 1;}
-
-    else if (*counter > 1 && *power == 0) //special condition for !first prime number
-        printf(" x %d",divisor);
-    else
-        printf(" x %d",divisor);
-}
-
-void  check_for_prime(bool A[]) {
-    int sqr = (int)sqrt(UPPER_BOUND);
-    for(int i = 2; i < sqr; i++) {
-        if(A[i] == true) {
-            for(int j = i*i; j < UPPER_BOUND; j +=i)
-                A[j] = false;
+            if(first_number) {
+                printf("%d",B[i]);
+                if(power > 1)
+                    printf("^%d",power);
+                first_number = false;
+            }else {
+                printf(" x %d", B[i]);
+                if(power > 1)
+                    printf("^%d",power);
+            }
         }
     }
+    putchar('\n');
 }
-
-void array_of_primes(int B[], bool A[]) {
-    int index = 0;
-    for( int i = 0; i < UPPER_BOUND; i++) {
-        if(A[i] == true) {
-            B[index] = i;
-            index ++;
-        }
-    }
-}
-
-
-
 // let A be an array of Boolean values, indexed by integers 2 to n,
 //   initially all set to true.
 //
@@ -159,4 +163,7 @@ void array_of_primes(int B[], bool A[]) {
 //               set A[j] := false
 //
 //   return all i such that A[i] is true.
+//
+//
+
 
